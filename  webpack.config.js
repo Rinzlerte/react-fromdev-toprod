57e4d3d080fdc6a683f07d742d312cc61721d1e9
Serg Nazarchuk <sergnazarchuk@MacBook-Pro-Serg.local>
2023-09-27 19:04:09 +0300

    1p

---  webpack.config.js
+++  webpack.config.js
@@ -3,14 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack'); 
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: path.resolve(__dirname, 'src' , 'index.js'),
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: '[name].[contenthash].js',
         clean: true
     },
     plugins: [
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
+        new webpack.ProgressPlugin()
     ],
 };
