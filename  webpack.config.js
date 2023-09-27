9415f4e0dec86b55c30ff76faf69fc0dd6000581
Serg Nazarchuk <sergnazarchuk@MacBook-Pro-Serg.local>
2023-09-27 18:11:16 +0300

    commit

---  webpack.config.js
+++  webpack.config.js
@@ -1,9 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack'); 
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src' , 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
 };
